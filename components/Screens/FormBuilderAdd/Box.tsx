69cc37bab58519810748fc7020b4f898c900b0d6
Maaz Nadeem <maaz.nadeem@qwertyexperts.com>
2023-12-13 16:44:12 +0500

    form

--- components/Screens/FormBuilderAdd/Box.tsx
+++ components/Screens/FormBuilderAdd/Box.tsx
@@ -1,54 +1,84 @@
-import type { FC } from 'react';
-import type { DragSourceMonitor } from 'react-dnd';
-import { useDrag } from 'react-dnd'
-import Image from 'next/image';
-import styles from './styles.module.css'
+import type { FC } from "react";
+import React, { useState } from "react";
+import type { DragSourceMonitor } from "react-dnd";
+import { useDrag } from "react-dnd";
+import Image from "next/image";
+import { Button, Modal } from "antd";
+import styles from "./styles.module.css";
 
 export interface BoxProps {
-  name: string
-  imagePath: string
+  name: string;
+  imagePath: string;
 }
 
 interface DropResult {
-  allowedDropEffect: string
-  dropEffect: string
-  name: string
+  allowedDropEffect: string;
+  dropEffect: string;
+  name: string;
 }
 
 export const Box: FC<BoxProps> = ({ name, imagePath }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const [{ opacity }, drag] = useDrag(
     () => ({
-      type: 'box',
+      type: "box",
       item: { name },
       end(item, monitor) {
-        const dropResult = monitor.getDropResult() as DropResult
+        const dropResult = monitor.getDropResult() as DropResult;
         if (item && dropResult) {
-          let alertMessage = ''
+          let alertMessage = "";
           const isDropAllowed =
-            dropResult.allowedDropEffect === 'any' ||
-            dropResult.allowedDropEffect === dropResult.dropEffect
+            dropResult.allowedDropEffect === "any" ||
+            dropResult.allowedDropEffect === dropResult.dropEffect;
 
           if (isDropAllowed) {
-            const isCopyAction = dropResult.dropEffect === 'copy'
-            const actionName = isCopyAction ? 'copied' : 'moved'
-            alertMessage = `You ${actionName} ${item.name} into ${dropResult.name}!`
+            const isCopyAction = dropResult.dropEffect === "copy";
+            const actionName = isCopyAction ? "copied" : "moved";
+            alertMessage = `You are useless`;
           } else {
-            alertMessage = `You cannot ${dropResult.dropEffect} an item into the ${dropResult.name}`
+            alertMessage = `You cannot ${dropResult.dropEffect} an item into the ${dropResult.name}`;
           }
-          alert(alertMessage)
+          alert(alertMessage);
         }
       },
       collect: (monitor: DragSourceMonitor) => ({
         opacity: monitor.isDragging() ? 0.4 : 1,
       }),
     }),
-    [name],
-  )
+    [name]
+  );
 
   return (
-    <div ref={drag} style={{ opacity }} className={styles.boxDrag}>
-      <Image src={imagePath} height={20} alt="" /> &nbsp;
-      {name}
-    </div>
-  )
-}
+    <>
+      <div ref={drag} style={{ opacity }} className={styles.boxDrag}>
+        <Image src={imagePath} height={20} alt="" /> &nbsp;
+        {name}
+      </div>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
--- components/Screens/FormBuilderAdd/Container.tsx
+++ components/Screens/FormBuilderAdd/Container.tsx
@@ -1,22 +1,31 @@
-import { Box } from './Box'
-import { Col, Flex, Input, Row } from 'antd'
-import { useDrop } from 'react-dnd'
-import { useState } from 'react'
-import styles from './styles.module.css'
-import FloatLabel from '@/components/ReusableComponents/FloatLabel'
-import { CALENDER, CHECK, DOCUMENT, DOCUMENTTXT, DOCUMENTUPLOAD, KEYBOARD, RECORD, TICKSQUARE } from '@/constants/images'
+import { Box } from "./Box";
+import { Col, Flex, Input, Row } from "antd";
+import { useDrop } from "react-dnd";
+import { useState } from "react";
+import styles from "./styles.module.css";
+import FloatLabel from "@/components/ReusableComponents/FloatLabel";
+import {
+  CALENDER,
+  CHECK,
+  DOCUMENT,
+  DOCUMENTTXT,
+  DOCUMENTUPLOAD,
+  KEYBOARD,
+  RECORD,
+  TICKSQUARE,
+} from "@/constants/images";
 
 export default function Container() {
   const colStyle = {
-    padding: '8px'
+    padding: "8px",
   };
 
-  const [allowedDropEffect, setAllowedDropEffect] = useState('move');
+  const [allowedDropEffect, setAllowedDropEffect] = useState("move");
   const [projectTitle, setproJectTitle] = useState("");
 
   const [, drop] = useDrop(
     () => ({
-      accept:'box',
+      accept: "box",
       drop: () => ({
         name: `${allowedDropEffect} Dustbin`,
         allowedDropEffect,
@@ -26,56 +35,154 @@ export default function Container() {
         canDrop: monitor.canDrop(),
       }),
     }),
-    [allowedDropEffect],
-  )
+    [allowedDropEffect]
+  );
 
   return (
     <Row>
       <Col xxl={14} lg={14} md={24} sm={24} xs={24} style={colStyle}>
         <Flex className={styles.detailsSectionLeft} vertical>
-          <div style={{ marginLeft: 5, marginRight: 5 }}>
+          <div
+            style={{ marginLeft: 5, marginRight: 5, marginTop: 5 }}
+            className="mb-5"
+          >
             <FloatLabel label="Project Title" value={projectTitle}>
-              <Input style={{ height: "48px" }} value={projectTitle} onChange={e => setproJectTitle(e.target.value)} />
+              <Input
+                style={{ height: "48px" }}
+                value={projectTitle}
+                onChange={(e) => setproJectTitle(e.target.value)}
+              />
             </FloatLabel>
           </div>
           <Row>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Text Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Text Area" imagePath={DOCUMENTTXT} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Dropdown Menu" imagePath={DOCUMENT} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Check Box" imagePath={TICKSQUARE} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Radio Button" imagePath={RECORD} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Date Picker" imagePath={CALENDER} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="File Upload" imagePath={DOCUMENTUPLOAD} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Email Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Number Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Password Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Phone Number Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="URL Input" imagePath={CHECK} />
             </Col>
-            <Col xxl={12} lg={12} md={24} sm={24} xs={24} style={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Col
+              xxl={12}
+              lg={12}
+              md={24}
+              sm={24}
+              xs={24}
+              style={{ paddingLeft: 5, paddingRight: 5 }}
+            >
               <Box name="Button" imagePath={KEYBOARD} />
             </Col>
           </Row>
@@ -84,11 +191,9 @@ export default function Container() {
 
       <Col xxl={10} lg={10} md={24} sm={24} xs={24} style={colStyle} ref={drop}>
         <Flex className={styles.detailsSection} vertical>
-          <div>
-            Drag & Drop the options here
-          </div>
+          <div>Drag & Drop the options here</div>
         </Flex>
       </Col>
     </Row>
-  )
+  );
 }
